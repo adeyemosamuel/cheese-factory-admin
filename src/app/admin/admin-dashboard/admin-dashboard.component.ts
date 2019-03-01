e9d6f82155b95a69834170623c4b3d8a289037d4
Michael Akobundu <P7543@Ogunyomis-MacBook-Pro-2.local>
2019-03-01 16:27:17 +0100

    update

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -9,27 +9,13 @@ import { Router, ActivatedRoute} from '@angular/router';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  UserId: any;
-  spinner = false;
-  UserDetails: any = [];
 
   constructor( private activatedRoute: ActivatedRoute, private router: Router, private server: ServerService) { }
 
  async ngOnInit() {
-  this.activatedRoute.params.subscribe((Username) => {
-    this.UserId = Username.id;
-    this.fetchUserDetails(this.UserId);
-  });
+
   }
 
-  async fetchUserDetails(id) {
-    this.toggleSpinner();
-    const response = await this.server.getService(`GetUserProfile/${id}`); // `template string
-    this.toggleSpinner();
-   }
 
-   toggleSpinner(): void {
-    this.spinner = !this.spinner;
-  }
 
 }
